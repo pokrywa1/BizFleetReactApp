335137c001ceb9299f14f4d36c212f13952c94c9
hubertpokrywka <h.pokrywka@wp.pl>
2023-10-30 17:48:38 +0100

    remove button from modal

--- src/components/user/cars/modals/CarsAddFormModal.tsx
+++ src/components/user/cars/modals/CarsAddFormModal.tsx
@@ -5,7 +5,6 @@ import {
   TPostCarSchema,
 } from '../../../../app/api/user/cars/postCar.tsx'
 import InputText from '../../../common/Inputs/InputText.tsx'
-import { Button } from '../../../common/Buttons/Button.tsx'
 import { Stack } from '@mantine/core'
 import CarImageDropzone from '../CarImageDropzone.tsx'
 import { useGetCars } from '../../../../app/api/user/cars/getCars.tsx'
@@ -44,7 +43,6 @@ const CarsAddFormModal = ({ opened, onClose }: CarsAddFormModalProps) => {
         <InputText name={inputsNames.model} placeholder={'Model'} />
         <InputText name={inputsNames.licensePlate} placeholder={'Rejestracja'} />
         <InputText name={inputsNames.year} placeholder={'Rocznik'} />
-        <Button type={'submit'}>Zatwierdź</Button>
       </Stack>
     </FormModal>
   )
